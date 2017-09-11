95195963ac7b95048579b4b22ee6d7a161402d37
Sándor Vass <vasssandor@hotmail.com>
2017-09-11 16:45:56 +0200

    Finished the fifteenth exercise

--- week-02/Day-01/Exercise-01/src/HelloUser.java
+++ week-02/Day-01/Exercise-01/src/HelloUser.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class HelloUser {
   public static void main(String[] args) {
     // Modify this program to greet user instead of the World!
     // The program should ask for the name of the user
-    System.out.println("Hello, World!");
+
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Hello! What is your name?");
+      String userinput1 = scanner.nextLine();
+      System.out.println("\"Hello \"" + userinput1);
+
+
+
+
+
+
   }
 }
--- week-02/Day-01/Exercise-01/src/SecondsInADay.java
+++ week-02/Day-01/Exercise-01/src/SecondsInADay.java
@@ -4,6 +4,7 @@ public class SecondsInADay {
     int currentMinutes = 34;
     int currentSeconds = 42;
 
+
     // Write a program that prints the remaining seconds (as an integer) from a
     // day if the current time is represented bt the variables
   }
