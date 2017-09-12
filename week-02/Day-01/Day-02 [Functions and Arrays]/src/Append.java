7591342761fb326b200e24c45df0394e14cafebc
Sándor Vass <vasssandor@hotmail.com>
2017-09-12 14:10:07 +0200

    Finished the first two exercises of [Functions and Arrays]

---
+++ week-02/Day-01/Day-02 [Functions and Arrays]/src/Append.java
@@ -0,0 +1,5 @@
+public class Append {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ week-02/Day-01/Day-02 [Functions and Arrays]/src/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+    public static void main(String[] args) {
+        String al = "Greenfox";
+        Greet(al);
+    }
+
+    public static void Greet(String input) {
+        System.out.println("Greetings dear, " + input);
+    }
+
+}
