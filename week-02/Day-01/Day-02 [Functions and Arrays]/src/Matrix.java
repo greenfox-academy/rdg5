0734c226790b200c4602c1e97d4b64e6b0df6a0f
Sándor Vass <vasssandor@hotmail.com>
2017-09-12 17:30:52 +0200

    End of the day, I have six remaining tasks

--- week-02/Day-01/Day-02 [Functions and Arrays]/src/Matrix.java
+++ week-02/Day-01/Day-02 [Functions and Arrays]/src/Matrix.java
@@ -1,5 +1,21 @@
 public class Matrix {
     public static void main(String[] args) {
-        
+       /* int[][] matrix = new int[4][4];
+        matrix = {{0,1  {0,1}}}
+        for (int i = 0; i < 1; i++) {
+            for (int j = 1; j < 1; j--) {
+                matrix[i][j] = i + j;
+            }
+        }
+        for (int[] a : matrix) {
+            for (int i : a) {
+                System.out.println(i + "\t");
+            }
+            System.out.println("\n");
+        }
+        */
+
+      int [] [] matrix = new int[4][4];
+      for (int i =0; i <= 1; i++);
     }
 }
