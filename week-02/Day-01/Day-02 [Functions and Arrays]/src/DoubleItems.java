b2b8b233a29904aecaa888f0e8084d895865f293
Sándor Vass <vasssandor@hotmail.com>
2017-09-13 13:06:15 +0200

    Finished the first exercise block, will start the second one. [I will return for other solves later on]

---
+++ week-02/Day-01/Day-02 [Functions and Arrays]/src/DoubleItems.java
@@ -0,0 +1,7 @@
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] ag = {3, 4, 5, 6, 7};
+
+
+    }
+}
---
+++ week-02/Day-03/src/TodoPrint.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TodoPrint {
+    public static void main(String... args){
+        String todoText = "- Buy milk\n";
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected outpt:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+        ArrayList<String> todoTask = new ArrayList<String>();
+        String task1 = "- Buy milk\n";
+        String task2 = "- Download games\n";
+        String task3 = "- Diablo";
+        todoTask.add(task1+task2+task3);
+        System.out.println(todoTask);
+    }
+}
