2aa3d9d6b18151ad5f6908adb3924c419589b35a
Sándor Vass <vasssandor@hotmail.com>
2017-09-14 13:57:01 +0200

    Starting the remaining tasks of Day-02 [Arrays and functions]

--- week-02/Day-01/Exercise-01/src/ParametricAverage.java
+++ week-02/Day-01/Exercise-01/src/ParametricAverage.java
@@ -11,21 +11,20 @@ public class ParametricAverage {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-      double sum = 0;
+    double sum = 0;
     System.out.println("Please enter the number of entries!");
     int number = scanner.nextInt();
 
-    for (int i = 1; i < number ; i++) {
+    for (int i = 1; i < number; i++) {
       System.out.println("Please input number " + i);
       sum += scanner.nextDouble();
-     }
-
-     System.out.println("Sum is: " + sum);
-     double average = sum / number;
-     System.out.println("The total average is: " +average);
-     }
     }
 
+    System.out.println("Sum is: " + sum);
+    double average = sum / number;
+    System.out.println("The total average is: " + average);
+  }
+}
 
 
-  
+
