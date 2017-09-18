be186d6ccd49bc21794a8169c8f353f4bc584e10
Sándor Vass <vasssandor@hotmail.com>
2017-09-18 12:44:37 +0200

    Answered Question Time

--- week-03/Trial Exam/src/OddFilter.java
+++ week-03/Trial Exam/src/OddFilter.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class OddFilter {
+
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every odd element from the orignal list
@@ -13,15 +14,15 @@ public class OddFilter {
 
   private static int oddFilter(int[] values) {
     int count = 0;
-    for (int i = 0; i < values.length ; i++) {
-      if (values[i] %2 !=0);{
+    for (int i = 0; i < values.length; i++) {
+      if (values[i] % 2 != 0) {
+        ;
+      }
+      {
         count++;
       }
     }
     return count;
-
   }
-
-
 }
 
