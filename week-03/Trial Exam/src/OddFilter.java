a97111928131d79623fb762ac37f3dfe957e50bb
Sándor Vass <vasssandor@hotmail.com>
2017-09-18 12:34:33 +0200

    Finished the OddFilter exercise

--- week-03/Trial Exam/src/OddFilter.java
+++ week-03/Trial Exam/src/OddFilter.java
@@ -1,22 +1,27 @@
-/*import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class OddFilter {
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every odd element from the orignal list
-    System.out.println(oddFilter(Arrays.asList(1, 2, 3, 4, 5)));
+    int[] numbers = {1, 2, 3, 4, 5};
+    System.out.println(oddFilter(numbers));
     // should print [1, 3, 5]
   }
 
-  private static int oddFilter(List<Integer> integers) {
-    int counter = 0;
-    for (int i = 0; i <= integers.size(); i++) {
-      if (integers.get(i) % 2 != 0) {
-        counter++;
+  private static int oddFilter(int[] values) {
+    int count = 0;
+    for (int i = 0; i < values.length ; i++) {
+      if (values[i] %2 !=0);{
+        count++;
       }
-    }return counter;
+    }
+    return count;
 
   }
-  }
-*/
+
+
+}
+
