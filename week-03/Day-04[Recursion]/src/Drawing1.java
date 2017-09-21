97e259ff0ad4249e6410ecdc41b4669afa79cf71
Sándor Vass <vasssandor@hotmail.com>
2017-09-21 17:30:45 +0200

    init drawing task, init string2, will finish later on

--- week-03/Day-04[Recursion]/src/Drawing1.java
+++ week-03/Day-04[Recursion]/src/Drawing1.java
@@ -1,42 +1,48 @@
-//import javax.swing.*;
-//
-//import java.awt.*;
-//
-//import static javax.swing.JFrame.EXIT_ON_CLOSE;
-//
-//public class Drawing1 {
-//
-//  public static void mainDraw(Graphics graphics) {
-//    graphics.setColor(Color.yellow);
-//    graphics.fillRect(0,0,600,600);
-//
-//
-//  }
+import javax.swing.*;
+
+import java.awt.*;
+
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Drawing1 {
+
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(Color.yellow);
+    graphics.fillRect(0, 0, 600, 600);
+
+    graphics.setColor(Color.black);
+    graphics.drawRect(200, 200, WIDTH / 3, HEIGHT / 3);
+    graphics.drawRect(400, 400, WIDTH / 3, HEIGHT / 3);
+    graphics.drawRect(600, 600, WIDTH / 3, HEIGHT / 3);
+
+  }
 //  public static void drawBot (int n, int x, int y, int w, int h){
 //    if (h <= min)
 //  }
-//
-//  //    Don't touch the code below
-//  static int WIDTH = 600;
-//  static int HEIGHT = 600;
-//
-//  public static void main(String[] args) {
-//    JFrame jFrame = new JFrame("Drawing");
-//    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
-//    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//    jFrame.add(new ImagePanel());
-//    jFrame.setLocationRelativeTo(null);
-//    jFrame.setVisible(true);
-//  }
-//
-//  static class ImagePanel extends JPanel {
-//
-//    @Override
-//    protected void paintComponent(Graphics graphics) {
-//      super.paintComponent(graphics);
-//      mainDraw(graphics);
-//
-//    }
-//  }
-//
-//}
+
+  //    Don't touch the code below
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
--- week-03/Day-04[Recursion]/src/String2.java
+++ week-03/Day-04[Recursion]/src/String2.java
@@ -1,17 +1,20 @@
-//public class String2 {
-//
-//  public static void main(String[] args) {
-//    System.out.println(charRemover("xerxes xaril xoxo", "x", ""));
-//
-//  }
-//
-//  public static String charRemover(String input, String target, String output) {
-//    int position = input.indexOf(output);
-//    if (position < 0) {
-//      return input;
-//    }
-//    return input.substring(0, position) + output + charRemover(
-//        input.substring(position + target.length()),
-//        target.replace(target, ""), output);
-//  }
-//}
+public class String2 {
+
+  public static void main(String[] args) {
+    System.out.println(charRemover("xerxes xaril xoxo", "x", ""));
+
+  }
+
+  public static String charRemover(String input, String target, String output) {
+    if (target.isEmpty()) {
+      return input;
+    }
+    int position = input.indexOf(output);
+    if (position < 0) {
+      return input;
+    }
+    return input.substring(0, position) + output + charRemover(
+        input.substring(position + target.length()),
+        target.replaceAll(target, ""), output);
+  }
+}
