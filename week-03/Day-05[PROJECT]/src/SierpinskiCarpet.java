114b1879993dffc0213a984db1a0b678584c627f
Sándor Vass <vasssandor@hotmail.com>
2017-09-22 10:02:42 +0200

    init SierpinskiCarpet base square

---
+++ week-03/Day-05[PROJECT]/src/SierpinskiCarpet.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class SierpinskiCarpet {
+
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(Color.black);
+    graphics.fillRect(200,200,200,200);
+
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
--- week-03/Day-05[PROJECT]/src/Triangles.java
+++ week-03/Day-05[PROJECT]/src/Triangles.java
@@ -1,34 +1,35 @@
-import javax.swing.*;
-
-import java.awt.*;
-
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
-public class Triangles {
-
-  public static void mainDraw(Graphics graphics) {
-
-  }
-
-  //    Don't touch the code below
-  static int WIDTH = 320;
-  static int HEIGHT = 343;
-
-  public static void main(String[] args) {
-    JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
-    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    jFrame.add(new ImagePanel());
-    jFrame.setLocationRelativeTo(null);
-    jFrame.setVisible(true);
-  }
-
-  static class ImagePanel extends JPanel {
-
-    @Override
-    protected void paintComponent(Graphics graphics) {
-      super.paintComponent(graphics);
-      mainDraw(graphics);
-    }
-  }
-}
+//import javax.swing.*;
+//
+//import java.awt.*;
+//
+//import static javax.swing.JFrame.EXIT_ON_CLOSE;
+//
+//public class Triangles {
+//
+//  public static void mainDraw(Graphics graphics) {
+//    graphics.
+//
+//  }
+//
+//  //    Don't touch the code below
+//  static int WIDTH = 600;
+//  static int HEIGHT = 600;
+//
+//  public static void main(String[] args) {
+//    JFrame jFrame = new JFrame("Drawing");
+//    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+//    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+//    jFrame.add(new ImagePanel());
+//    jFrame.setLocationRelativeTo(null);
+//    jFrame.setVisible(true);
+//  }
+//
+//  static class ImagePanel extends JPanel {
+//
+//    @Override
+//    protected void paintComponent(Graphics graphics) {
+//      super.paintComponent(graphics);
+//      mainDraw(graphics);
+//    }
+//  }
+//}
