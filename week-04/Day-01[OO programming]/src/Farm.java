217200996b13d0de68b3f7510df7d44c1453d83d
Sándor Vass <vasssandor@hotmail.com>
2017-09-25 17:24:29 +0200

    finished SharpieSet, started Farm, will finish at home.

---
+++ week-04/Day-01[OO programming]/src/Farm.java
@@ -0,0 +1,23 @@
+//import java.util.List;
+//
+//public class Farm {
+//
+//  public List<Animal> animals;
+//
+//
+//  public void breed() {
+//    animals.add(new Animal());
+//  }
+//
+//  public void slaughter() {
+//    animals.remove()
+//
+//        //fori which one is least hungry
+//    //remove that one
+//
+//  }
+//
+//  public static void main(String[] args) {
+//
+//  }
+//}
---
+++ week-04/Day-01[OO programming]/src/SharpieSet.java
@@ -0,0 +1,37 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class SharpieSet {
+//
+//  public List<Sharpie> sharpies;
+//  public static int sharpieCount;
+//
+//  public void SharpieSet() {
+//    sharpies = new ArrayList<>();
+//    sharpieCount = 0;
+//
+//  }
+//
+////  public void addThemSharpies(Sharpie theSharpie) {
+////    sharpies.add(theSharpie);
+////    sharpieCount = sharpies.size();
+////  }
+//
+//  public countUsable() {
+//    int ink;
+//    for (sharpies:
+//    ink > 0) {
+//      sharpieCount++;
+//
+//    }
+//  }
+//
+//  public removeTrash() {
+//
+//  }
+//
+//  public static void main(String[] args) {
+//
+//  }
+//
+//}
