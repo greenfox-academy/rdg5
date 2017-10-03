1e2e7ed2387745b204af500e11b7317a4bcaa08b
Sándor Vass <vasssandor@hotmail.com>
2017-10-03 12:00:09 +0200

    single mid tile drawn

---
+++ week-05/Day-02 [ScrumGame]/src/Board.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Board extends JComponent implements KeyListener {
+
+  int testBoxX;
+  int testBoxY;
+
+  public Board() {
+    testBoxX = 300;
+    testBoxY = 300;
+
+    // set the size of your draw board
+    setPreferredSize(new Dimension(720, 720));
+    setVisible(true);
+  }
+
+  @Override
+  public void paint(Graphics graphics) {
+    super.paint(graphics);
+//    graphics.fillRect(testBoxX, testBoxY, 100, 100);
+    // here you have a 720x720 canvas
+    // you can create and draw an image using the class below e.g.
+    PositionedImage image = new PositionedImage("./Assets/floor.png", 300, 300);
+    image.draw(graphics);
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // When the up or down keys hit, we change the position of our box
+    if (e.getKeyCode() == KeyEvent.VK_UP) {
+      testBoxY -= 100;
+    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+      testBoxY += 100;
+    }
+    // and redraw to have a new picture with the new coordinates
+    repaint();
+  }
+}
