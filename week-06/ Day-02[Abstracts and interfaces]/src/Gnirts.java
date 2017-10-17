01bb293078f83c3196831410c1fcc83532e5968b
Sándor Vass <vasssandor@hotmail.com>
2017-10-17 14:33:34 +0200

    3 methods concerning Gnirts done, starting Shifter

---
+++ week-06/ Day-02[Abstracts and interfaces]/src/Gnirts.java
@@ -0,0 +1,47 @@
+import java.util.stream.IntStream;
+
+public class Gnirts implements CharSequence {
+
+  String forTehWinz;
+
+  public Gnirts(String fTW) {
+    this.forTehWinz = fTW;
+  }
+
+//  public  String reverse(){
+//    this.forTehWinz = forTehWinz;
+//    char ch[]=forTehWinz.toCharArray();
+//    String reversed="";
+//    for(int i=ch.length-1;i>=0;i--){
+//      reversed+=ch[i];
+//    }
+//    return reversed;
+//  }
+
+  @Override
+  public int length() {
+    return forTehWinz.length();
+  }
+
+  @Override
+  public char charAt(int index) {
+    char ch[] = forTehWinz.toCharArray();
+    String reversed = "";
+    for (int i = ch.length - 1; i >= 0; i--) {
+      reversed += ch[i];
+    }
+    return reversed.charAt(index);
+  }
+
+  @Override
+  public CharSequence subSequence(int start, int end) {
+    char ch[] = forTehWinz.toCharArray();
+    String reversed = "";
+    for (int i = ch.length - 1; i >= 0; i--) {
+      reversed += ch[i];
+    }
+    return reversed.subSequence(start, end);
+  }
+
+
+}
--- week-06/ Day-02[Abstracts and interfaces]/src/Main.java
+++ week-06/ Day-02[Abstracts and interfaces]/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
 
   public static void main(String[] args) throws CloneNotSupportedException {
 
-    Student jóska = new Student("Lakatos József", 20, "male", "BME");
-    Student jóskaTheClone = (Student) jóska.clone();
+//    Student jóska = new Student("Lakatos József", 20, "male", "BME");
+//    Student jóskaTheClone = (Student) jóska.clone();
+
+    Gnirts g = new Gnirts("example");
+    System.out.println(g.length());
+    System.out.println(g.charAt(1));
+    System.out.println(g.subSequence(0,4));
   }
 }
