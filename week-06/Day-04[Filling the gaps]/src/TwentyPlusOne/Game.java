0090b812e3e0b69ce61d255a7cabe3f70d39436d
Sándor Vass <vasssandor@hotmail.com>
2017-10-19 16:48:54 +0200

    Opponent random score implemented

--- week-06/Day-04[Filling the gaps]/src/TwentyPlusOne/Game.java
+++ week-06/Day-04[Filling the gaps]/src/TwentyPlusOne/Game.java
@@ -1,8 +1,19 @@
 package TwentyPlusOne;
 
+import java.util.Scanner;
+
 public class Game {
 
   public static void main(String[] args) {
+      int opponentScore = (int) (Math.random() * ((21-15)+1))+15 ;
+      int playerScore = 0;
+      Deck d = new Deck();
+      d.shuffleDeck();
 
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Greetings. Let the games begin!");
+    System.out.println("Do you want to draw a card?");
+    
   }
 }
