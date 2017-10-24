cfc5ad81dffcb07be37de0fa32b65e36dde7e6c1
Sándor Vass <vasssandor@hotmail.com>
2017-10-24 13:08:02 +0200

    id incrementaton added with AtomicLong

--- week-07/helloworld/src/main/java/com/greenfox/rdg5/helloworld/controllers/HelloRESTController.java
+++ week-07/helloworld/src/main/java/com/greenfox/rdg5/helloworld/controllers/HelloRESTController.java
@@ -5,14 +5,17 @@ import static com.sun.corba.se.spi.activation.IIOP_CLEAR_TEXT.value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @RestController
 
 public class HelloRESTController {
+  AtomicLong counter = new AtomicLong();
 
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam("name") String name) {
-    Greeting g = new Greeting(1, "Hello " + name);
+    Greeting g = new Greeting(counter.incrementAndGet(), "Hello " + name);
     return g;
   }
 
