582812a2a6b87b95c18d1b0491e2cba0addaae23
Sándor Vass <vasssandor@hotmail.com>
2017-10-25 15:49:49 +0200

    refactored code, added new column with index number.

--- week-07/Day-03[BankOfSimba]/src/main/java/com/greenfox/rdg5/bankofsimba/controller/BankAccountController.java
+++ week-07/Day-03[BankOfSimba]/src/main/java/com/greenfox/rdg5/bankofsimba/controller/BankAccountController.java
@@ -10,31 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BankAccountController {
 
-  BankAccount simbaAcc = new BankAccount("Simba", 2000, "lion");
-  BankAccount zazuAcc = new BankAccount("Zazu", 6000, "red-billed hornbill");
-  BankAccount shenziAcc = new BankAccount("Shenzi", 500, "hyena");
-  BankAccount fredAcc = new BankAccount("Fred", 2000, "meerkat");
-  BankAccount pumbaaAcc = new BankAccount("Pumbaa", 13000, "warthog");
-
   ArrayList<BankAccount> accounts = new ArrayList<>();
+
+  public BankAccountController() {
+    accounts.add(new BankAccount("Simba", 2000, "lion"));
+    accounts.add(new BankAccount("Zazu", 6000, "red-billed hornbill"));
+    accounts.add(new BankAccount("Shenzi", 500, "hyena"));
+    accounts.add(new BankAccount("Fred", 2000, "meerkat"));
+    accounts.add(new BankAccount("Pumbaa", 13000, "warthog"));
+
+  }
 
   @RequestMapping(value = "/Exercise1")
   public String initBankAccount(Model model) {
-    model.addAttribute("name", simbaAcc.getName());
-    model.addAttribute("balance", simbaAcc.getBalance());
-    model.addAttribute("type", simbaAcc.getAnimalType());
-    model.addAttribute("currency", simbaAcc.getCurrency());
+    model.addAttribute("name", accounts.get(0).getName());
+    model.addAttribute("balance", accounts.get(0).getBalance());
+    model.addAttribute("type", accounts.get(0).getAnimalType());
+    model.addAttribute("currency", accounts.get(0).getCurrency());
     return "Exercise1";
   }
 
 
   @RequestMapping(value = "/Exercise5")
   public String fillMultipleAccounts(Model model) {
-    accounts.add(0, simbaAcc);
-    accounts.add(1, zazuAcc);
-    accounts.add(2, shenziAcc);
-    accounts.add(3, fredAcc);
-    accounts.add(4, pumbaaAcc);
     model.addAttribute("accounts", accounts);
     return "Exercise5";
   }
