fdd98d96b76f894f10d06e012910a3fa3957ac13
Sándor Vass <vasssandor@hotmail.com>
2017-10-27 11:13:46 +0200

    Html working, task reproduced as needed, ready to start the next part of the task

--- week-07/Day-05[Programmer Fox Club Project]/src/main/java/com/greenfox/rdg5/foxclub/FoxclubApplication.java
+++ week-07/Day-05[Programmer Fox Club Project]/src/main/java/com/greenfox/rdg5/foxclub/FoxclubApplication.java
@@ -2,11 +2,12 @@ package com.greenfox.rdg5.foxclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class FoxclubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoxclubApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FoxclubApplication.class, args);
+  }
 }
