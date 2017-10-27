e75bdcf716a6bf77f1cc378916c86c54deff125b
Sándor Vass <vasssandor@hotmail.com>
2017-10-27 10:47:48 +0200

    init project, ready to start the application

---
+++ week-07/Day-05[Programmer Fox Club Project]/src/main/java/com/greenfox/rdg5/foxclub/FoxclubApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.rdg5.foxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxclubApplication.class, args);
+	}
+}
