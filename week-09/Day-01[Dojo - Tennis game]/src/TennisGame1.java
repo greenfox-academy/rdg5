22a1621c17f6d598d4b2696c19177a5c508c554d
Sándor Vass <vasssandor@hotmail.com>
2017-11-06 17:30:48 +0100

    refactor() : create separate methods for checking score

--- week-09/Day-01[Dojo - Tennis game]/src/TennisGame1.java
+++ week-09/Day-01[Dojo - Tennis game]/src/TennisGame1.java
@@ -10,11 +10,13 @@ public class TennisGame1 implements TennisGame {
     player2 = new Player(player2Name);
   }
 
-  public void wonPoint(String currentPlayerName) {
-    if (currentPlayerName == player1.name) {
-      player1.score += 1;
+  public String getScore() {
+    if (player1.score == player2.score) {
+      return getScoreIfTie();
+    } else if (player1.score >= 4 || player2.score >= 4) {
+      return getScoreIfAboveForty();
     } else {
-      player2.score += 1;
+      return getScoreIfBelowForty();
     }
   }
 
@@ -34,40 +36,41 @@ public class TennisGame1 implements TennisGame {
     return result;
   }
 
-  public String getScore() {
-    String uniqueResult = "";
-    if (player1.score == player2.score) {
-      return getScoreIfTie();
-
-    } else if (player1.score >= 4 || player2.score >= 4) {
-      int minusResult = player1.score - player2.score;
-      if (minusResult == 1) {
-        uniqueResult = "Advantage player1";
-      } else if (minusResult == -1) {
-        uniqueResult = "Advantage player2";
-      } else if (minusResult >= 2) {
-        uniqueResult = "Win for player1";
-      } else {
-        uniqueResult = "Win for player2";
-      }
+  public String getScoreIfAboveForty() {
+    String result;
+    int minusResult = player1.score - player2.score;
+    if (minusResult == 1) {
+      result = "Advantage player1";
+    } else if (minusResult == -1) {
+      result = "Advantage player2";
+    } else if (minusResult >= 2) {
+      result = "Win for player1";
     } else {
+      result = "Win for player2";
+    }
+    return result;
+  }
 
-      for (int i = 0; i < 4; i++) {
-        if (player1.score == i) {
-          player1.scoreAsText += namesForScores[i];
-        }
-        if (player2.score == i) {
-          player2.scoreAsText += namesForScores[i];
-        }
+  public String getScoreIfBelowForty() {
+    for (int i = 0; i < 4; i++) {
+      if (player1.score == i) {
+        player1.scoreAsText += namesForScores[i];
+      }
+      if (player2.score == i) {
+        player2.scoreAsText += namesForScores[i];
       }
     }
-    if (uniqueResult != "") {
-      return uniqueResult;
+    return player1.scoreAsText + "-" + player2.scoreAsText;
+  }
+
+  public void wonPoint(String currentPlayerName) {
+    if (currentPlayerName == player1.name) {
+      player1.score += 1;
     } else {
-      uniqueResult = player1.scoreAsText + "-" + player2.scoreAsText;
-    } return  uniqueResult;
+      player2.score += 1;
     }
   }
+}
 
 
 
