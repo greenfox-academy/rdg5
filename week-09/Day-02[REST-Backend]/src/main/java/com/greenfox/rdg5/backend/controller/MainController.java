9ac0dcf97d0a414d2e3910f519c7d6117d0d05e6
Sándor Vass <vasssandor@hotmail.com>
2017-11-07 11:24:26 +0100

    feat(MainController) : static content init, ready to proceed withe second task

--- week-09/Day-02[REST-Backend]/src/main/java/com/greenfox/rdg5/backend/controller/MainController.java
+++ week-09/Day-02[REST-Backend]/src/main/java/com/greenfox/rdg5/backend/controller/MainController.java
@@ -1,5 +1,17 @@
 package com.greenfox.rdg5.backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping(value = "/")
 public class MainController {
+
+  @RequestMapping(value = "/")
+  public String getIndex(Model model){
+      return "index";
+  }
 
 }
