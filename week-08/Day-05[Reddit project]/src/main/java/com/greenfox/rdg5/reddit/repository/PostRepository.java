1e76a829846feee6ba3740eb7770d90a3e654c89
rdg5 <vasssandor@hotmail.com>
2018-01-14 18:55:32 +0100

    refactor(PostRepository) : add custom query for final exam

--- week-08/Day-05[Reddit project]/src/main/java/com/greenfox/rdg5/reddit/repository/PostRepository.java
+++ week-08/Day-05[Reddit project]/src/main/java/com/greenfox/rdg5/reddit/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package com.greenfox.rdg5.reddit.repository;
 
 import com.greenfox.rdg5.reddit.model.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+    @Query(value = "SELECT * FROM post ORDER BY post_rank DESC", nativeQuery = true)
+    public List<Post> listDesc();
 
 }
